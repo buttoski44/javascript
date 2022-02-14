e8fb0949a9109ca155467457e8867ee2856ffb6c
vishal sangole <wishalsangole4444@gmail.com>
2022-02-14 11:16:31 +0530

    basics

---
+++ basicsCheatsheet.js
@@ -0,0 +1,32 @@
+// Numbers:
+1;
+-99;
+0.345345;
+
+// java script has one number type
+// it covers positive No. , negative no. ,whole and decimal numbers 
+
+// Nan = 0/0 or 0+Nan 
+
+//Making variables with let:
+// syntax : let varname = value 
+let numberOfFriends = 1;
+
+
+//Incrementing:
+numberOfFriends += 3; //numberOfFriends is now 4
+// by 1:numberOfFriends++
+
+// Variables with const
+const minimumAge = 21; //CANNOT REASSIGN!
+
+//Booleans - true or false values
+true;
+false;
+let isHappy = true;
+
+//Naming Conventions
+// Use upper camel-cased names:
+let numberOfChickens = 6; //GOOD
+// NOT THE JS WAY:
+// let number_of_chickens = 6;
